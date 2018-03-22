f64bb0dc7419a88d0b6ca6698f208fbfcb1163b7
“Meu <thiagocarvalhotc11@hotmail.com>
2018-03-22 10:44:39 -0300

    Parte Final Da Avaliação

--- src/main/java/com/aulapabloramon/av0001/SpringBootAv0001Application.java
+++ src/main/java/com/aulapabloramon/av0001/SpringBootAv0001Application.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.aulapabloramon.av0001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
