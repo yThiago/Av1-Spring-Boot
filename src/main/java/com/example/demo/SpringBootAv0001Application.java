2b1f2f47edcfa94a930a5ee10f52a0dd03e338d4
“Meu <thiagocarvalhotc11@hotmail.com>
2018-03-22 10:17:06 -0300

    Primeira Parte da Avaliacao

---
+++ src/main/java/com/example/demo/SpringBootAv0001Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAv0001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAv0001Application.class, args);
+	}
+}
